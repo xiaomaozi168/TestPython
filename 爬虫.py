0e364320ac05625f087f9d5b16b9664751c6bb8f
lvzhengmao <zm349018250@qq.com>
2020-02-23 15:29:46 +0800

    爬虫更新

--- 爬虫.py
+++ 爬虫.py
@@ -1,1 +1,22 @@
-import re
+import urllib.request
+import random
+data = urllib.request.urlopen("https://www.jd.com/").read().decode("utf-8","ignore")
+print(data)
+
+uapools=[
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
+    "",
+    ""
+]
+def UA():
+    opener = urllib.request.build_opener()
+    thisua = random.choice(uapools)
+    ua = ("User-Agent",thisua)
+    opener.addheaders = [ua]
+    urllib.request.install_opener(opener)
+    print("当前使用的UA："+str(thisua))
+
+for i in range(0,10):
+    print(i)
+    UA()
+    data = urllib.request.urlopen("").read().decode("utf-8","ignore")
