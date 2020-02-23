0abfb335b9928dea37e38ab909d3757e98990425
lvzhengmao <zm349018250@qq.com>
2020-02-23 16:04:42 +0800

    更新

--- 爬虫.py
+++ 爬虫.py
@@ -1,12 +1,12 @@
 import urllib.request
 import random
+
 data = urllib.request.urlopen("https://www.jd.com/").read().decode("utf-8","ignore")
-print(data)
+#print(data)
 
 uapools=[
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
-    "",
-    ""
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363",
 ]
 def UA():
     opener = urllib.request.build_opener()
@@ -19,4 +19,4 @@ def UA():
 for i in range(0,10):
     print(i)
     UA()
-    data = urllib.request.urlopen("").read().decode("utf-8","ignore")
+    data = urllib.request.urlopen("https://www.jd.com/").read().decode("utf-8","ignore")
